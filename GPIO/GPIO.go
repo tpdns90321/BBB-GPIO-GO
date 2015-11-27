9c33e7c8c87cfea335777dac9368e427588f8fc8
kang <tpdns9032100@gmail.com>
2015-11-27 13:06:22 +0900

    testing initial commit

--- GPIO/GPIO.go
+++ GPIO/GPIO.go
@@ -7,7 +7,7 @@ package BBB_GPIO
 
 type bbb_gpio struct {
         pin       [][]int  //Pin name ex.P8_13 Pin(9,12)
-        pin_state []pin_data
+        pin_state map[int]pin_data
         state int
 }
 
@@ -38,7 +38,7 @@ var mode = map[int]string{
 }
 
 //pin allocate
-var pin_map = map[int][]int{
+var pin_map = map[int]*[]int{
         8: []int{0, 0, 0, 38, 39, 34, 35, 66, 67, 69, 68, 45, 44, 23, 26, 47, 46, 27, 65, 22, 63, 62, 37, 36, 33, 32, 61, 86, 88, 87, 89, 10, 11, 9, 81, 8, 80, 78, 79, 76, 77, 74, 75, 72, 73, 70, 71},
         9: []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30, 60, 31, 50, 48, 51, 5, 4, 0, 0, 3, 2, 49, 15, 117, 14, 115, 123, 121, 122, 120, 0, 0, 0, 0, 0, 0, 0, 0, 20, 7, 0, 0, 0, 0},
 }
@@ -48,8 +48,8 @@ var gpio bbb_gpio = bbb_gpio{}
 func (e *bbb_gpio) Error() string{
 	if e.state == ERROR{
 		return "BBB_GPIO Error"
-	}
-	return ""
+		}
+	return "BBB_GPIO Exception"
 }
 
 func Start()  (*bbb_gpio,error){
@@ -57,7 +57,9 @@ func Start()  (*bbb_gpio,error){
                 gpio.pin = make([][]int,10)
                 gpio.pin[8] = pin_map[8]
                 gpio.pin[9] = pin_map[9]
-                
-        }
-        return gpio,nil
+                gpio.pin_state = make(map[int]pin_data)
+        }else {
+			return nil, &gpio
+		}
+        return &gpio, nil
 }
